f79341e530932db2c40f4d4d3183501e93a9d162
Raymond Zwarts <rzwarts@gmail.com>
2026-04-17 14:47:40 +0200

    Frontend issue with /api

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
+const apiProxyTarget = process.env.VITE_API_PROXY_TARGET || 'http://localhost:8000';
+
 export default defineConfig({
   plugins: [sveltekit()],
   server: {
     proxy: {
-      '/api': 'http://localhost:8000'
+      '/api': apiProxyTarget
     }
   }
 });
